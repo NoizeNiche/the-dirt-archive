d5786ef9f10fc8eb1c3e512ac9e262d6ff1e9a3a
NoizeNiche <noizenicheig@gmail.com>
2026-09-14 02:23:29 -0700

    Add Phase 34 historical pedal image discovery leads

---
+++ public/catalog-thumbnails-34.js
@@ -0,0 +1,84 @@
+(() => {
+  window.DIRT_MEDIA = Object.assign(window.DIRT_MEDIA || {}, {
+    'Mojo Hand FX 1978 - OpAmp Fuzz': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/mojohand_1978_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/mojohand/1978',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Mojo Hand FX 1979 Fuzz': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/mojohand_1979_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/mojohand/1979',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Mojo Hand FX Buzzaround': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/mojohand_buzzaround_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/mojohand/buzzaround',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Mojo Hand FX Bluebonnet - Overdrive': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/mojohand_bluebonnet_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/mojohand/bluebonnet',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Mojo Hand FX Rook - Overdrive': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/mojohand_rook_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/mojohand/rook',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'DenTone Electronics 900 Pound Violin': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/dentone_900poundviolin_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/dentone/900poundviolin',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'DenTone Electronics Buzzaround': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/dentone_buzzaround_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/dentone/buzzaround',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'DenTone Electronics Fuzzrite': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/dentone_fuzzrite_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/dentone/fuzzrite',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'DenTone Electronics FY-2 Companion Fuzz': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/dentone_fy2_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/dentone/fy2',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Erafuzz Buzzaround - Germanium Fuzz Replica': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/erafuzz_buzzaround_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/erafuzz/buzzaround',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Erafuzz Civil War - Silicon Fuzz Machine': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/erafuzz_civilwar_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/erafuzz/civilwar',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Erafuzz The Italian V - 1966 Tone Bender Replica': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/erafuzz_italianv_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/erafuzz/italianv',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Ghost Effects Buzzaround': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/ghost_buzzaround_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/ghost/buzzaround',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Ghost Effects Ghost Bender MkI': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/ghost_ghostbender_mki_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/ghost/ghostbender/mk1',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Ghost Effects Ghost Bender MkII': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/ghost_ghostbender_mkii_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/ghost/ghostbender/mk2',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    },
+    'Ghost Effects Doom Bender MkI': {
+      src: 'https://files.effectsdatabase.com/gear/thumbs/ghost_doombender_mki_001.jpg',
+      page: 'https://www.effectsdatabase.com/model/ghost/doombender/mk1',
+      credit: 'Effects Database / source uploader', rights_status: 'Reference-only', public_use_decision: 'pending'
+    }
+  });
+})();
