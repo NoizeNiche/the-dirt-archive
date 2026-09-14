68185b0220c31867b7b926f0524be9ec9ea54d9d
NoizeNiche <noizenicheig@gmail.com>
2026-09-14 03:44:04 -0700

    Create structured visual specimen registry v1

---
+++ public/catalog-specimens-01.js
@@ -0,0 +1,106 @@
+(() => {
+  // Structured specimen registry. Add a new object for each independently
+  // documented photograph or visual specimen. Rights status is explicit.
+  window.DIRT_SPECIMENS = [
+    {
+      title: 'Fuzz Face',
+      role: 'Primary example',
+      era: '1960s–1970s',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Dallas_Arbiter_Fuzz_Face.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:Dallas_Arbiter_Fuzz_Face.jpg',
+      credit: 'sploshette · CC BY 2.0',
+      rights: 'Cleared',
+      caption: 'Dallas-Arbiter style round enclosure example.'
+    },
+    {
+      title: 'Tone Bender',
+      role: 'Primary example',
+      era: '1960s',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/1966_VOX_TONE_BENDER_FUZZ.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:1966_VOX_TONE_BENDER_FUZZ.jpg',
+      credit: 'Wikimedia Commons · CC BY 2.0',
+      rights: 'Cleared',
+      caption: 'Early Vox-associated Tone Bender specimen.'
+    },
+    {
+      title: 'Big Muff Pi',
+      role: 'Primary example',
+      era: '1969–1970s',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Electro_Harmonix_Big_Muff.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:Electro_Harmonix_Big_Muff.jpg',
+      credit: 'Skimel · CC BY-SA 4.0',
+      rights: 'Cleared',
+      caption: 'Electro-Harmonix Big Muff Pi reference specimen.'
+    },
+    {
+      title: 'TS808 Tube Screamer',
+      role: 'Primary example',
+      era: '1979–1981',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Ibanez_TS808_Tube_Screamer_%2848588080527%29_cropped.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:Ibanez_TS808_Tube_Screamer_%2848588080527%29_%28cropped%29.jpg',
+      credit: 'Guitar Chalk · CC BY 2.0',
+      rights: 'Cleared',
+      caption: 'Ibanez TS808 specimen used as an identification reference.'
+    },
+    {
+      title: 'RAT',
+      role: 'Primary example',
+      era: '1978–1980s',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Proco-rat.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:Proco-rat.jpg',
+      credit: 'Jazzman · Wikimedia Commons · see file page for license',
+      rights: 'Cleared',
+      caption: 'Early ProCo RAT enclosure example.'
+    },
+    {
+      title: 'MXR M-104 Distortion+',
+      role: 'Primary example',
+      era: '1970s',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/1979_MXR_Distortion_%2B.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:1979_MXR_Distortion_%2B.jpg',
+      credit: 'Wikimedia Commons · licensed image, see file page',
+      rights: 'Cleared',
+      caption: '1979 MXR Distortion+ specimen.'
+    },
+    {
+      title: 'BOSS DS-1 Distortion',
+      role: 'Primary example',
+      era: '1978–present',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Boss-DS-1.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:Boss-DS-1.jpg',
+      credit: 'Matt Eason · CC BY-SA 3.0 / GFDL',
+      rights: 'Cleared',
+      caption: 'Classic orange BOSS DS-1 specimen.'
+    },
+    {
+      title: 'Vox Tone Bender',
+      role: 'OEM example',
+      era: '1960s',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Vox_Tone_Bender.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:Vox_Tone_Bender.jpg',
+      credit: 'Johann Burkard · CC BY 2.0',
+      rights: 'Cleared',
+      caption: 'Vox-branded Tone Bender specimen.'
+    },
+    {
+      title: 'Ibanez TS-9 Tube Screamer',
+      role: 'Successor example',
+      era: '1981–1985',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Ibanez_ts9_tube_screamer.jpg',
+      page: 'https://commons.wikimedia.org/wiki/File:Ibanez_ts9_tube_screamer.jpg',
+      credit: 'Mataresephotos · CC BY 3.0 US',
+      rights: 'Cleared',
+      caption: 'TS-9 enclosure and control layout reference specimen.'
+    },
+    {
+      title: 'Colorsound Supa Tonebender',
+      role: 'Primary example',
+      era: '1970s',
+      src: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Sola_Sound_Colorsound_Supa_Tonebender,_from_1974.png',
+      page: 'https://commons.wikimedia.org/wiki/File:Sola_Sound_Colorsound_Supa_Tonebender,_from_1974.png',
+      credit: 'Wikimedia Commons · licensed image, see file page',
+      rights: 'Cleared',
+      caption: '1974 Supa Tonebender specimen.'
+    }
+  ];
+})();
