342766cf515cbe8013823d4621e615c930a58a47
NoizeNiche <noizenicheig@gmail.com>
2026-09-26 13:30:16 -0700

    Clean catalog card photo placeholders

--- assets/js/archive-index.js
+++ assets/js/archive-index.js
@@ -293,7 +293,7 @@ function render(){
         const img=pedalImages.get(entryKey(x));
         const media=img&&img.image
           ? '<div class="cardMedia"><img class="cardImage" src="'+esc(img.image)+'" alt="'+esc(x.company+' '+x.pedal)+' pedal" loading="lazy" referrerpolicy="no-referrer" onerror="this.hidden=true;this.nextElementSibling.hidden=false"><div class="cardPlaceholder" hidden>No Photo Archived</div></div>'
-          : '<div class="cardPlaceholder">No Photo Archived</div>';
+          : '<div class="cardPlaceholder" aria-label="Photo unavailable"></div>';
         return '<a class="card" href="'+slugParams(x)+'">'+
           media+
           '<span class="cardBody">'+
